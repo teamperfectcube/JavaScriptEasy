bdbe95e1ee426f16dbd2130b7e22c6ae279b7233
aboutgaurav <gaurav.kumar24@outlook.com>
2017-12-23 22:53:43 +0530

    initial commit

---
+++ app/src/main/java/com/itsgaurav/javascripteasy/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.itsgaurav.javascripteasy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gaurav on 23/12/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
